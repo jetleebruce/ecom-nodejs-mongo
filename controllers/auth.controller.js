5103972c8c66903c62b941b8d3e39794f4095986
Modin Sergei <jetleebruce@gmail.com>
2021-11-13 12:15:36 +0300

    add sign up function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,9 @@
 function getSignup(req, res) {
-   res.render('customer/auth/signup')
+  res.render('customer/auth/signup')
+}
+
+function signup(req, res) {
+  
 }
 
 function getLogin(req, res) {
@@ -8,5 +12,6 @@ function getLogin(req, res) {
 
 module.exports = {
   getSignup: getSignup,
-  getLogin: getLogin
+  getLogin: getLogin,
+  signup: signup
 }
