d579a094511a1e4d8ad2339280944090826ab37f
Modin Sergei <jetleebruce@gmail.com>
2021-11-18 11:15:48 +0300

    error handling

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 const express = require('express');
+const csrf = require('csurf');
 
 const db = require('./data/database');
-const authRoutes = require('./routes/auth.routes')
+const addCsrfTokenMiddleware = require('./middleware/csrf-token');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const authRoutes = require('./routes/auth.routes');
+
+
 
 const app = express();
 
@@ -13,7 +18,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+app.use(csrf());
+app.use(addCsrfTokenMiddleware);
+
 app.use(authRoutes);
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
 .then(function() {
