a14fb3276b7de1683d1f77bb2959750fc762262e
Modin Sergei <jetleebruce@gmail.com>
2021-11-18 14:08:16 +0300

    configure session

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const csrf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middleware/csrf-token');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authRoutes = require('./routes/auth.routes');
-
-
 
 const app = express();
 
@@ -18,7 +18,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig))
 app.use(csrf());
+
 app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
